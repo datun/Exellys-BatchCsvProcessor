162f16bf89c92641a038bc448784134014d72e8e
datun <denizatun@gmail.com>
2023-05-04 15:01:12 +0200

    Started the date selection feature. It is pain.

--- csv-loader.py
+++ csv-loader.py
@@ -3,6 +3,7 @@
 import numpy as np
 import pandas as pd
 import os
+import re
 
 def csv_2_list(csv_list):
     data_list = []  # Create a list with csv loaded as pandas
@@ -53,39 +54,60 @@ def input_checker(data_list, file_names):
             print("Input is not in correct form!\n")
             
 def csv_date_range(data_list):
-    
+    date_range = []
     while true:
+        # Choose granularity for date filtering
+        print("Please choose granularity of date filtering\n
+              0: Year Only (e.g. From 2000 To 2023)\n
+              1: Year+Month (e.g. From 2000-12 To 2012-06\n
+              2: Year+Month+Day (e.g. From 2000-12-12 To 2012-05-01"        
+        while true:
+            selection = input("Type your choice: ")
+            if int(selection) not in range(3):
+              print("Invalid selection")
+            else:
+              break
+
+        # Get start and end dates with accordance to the granularity
         check = 0
         print("Please input range of dates for filtering\n
-              Format your date input as YYYY-MM-DD or YYYY-MM or YYYY\n
-              If you want to have no upper or lower limit, input -1")
+              If you want to have no upper or lower limit, fill the date with 0\n
+              e.g. 2012, 2020-00, 0000, 1999-12-00")
+
+        # Janky way of repeating this process, turn it into a function later
         if not check:
             date_in = input ("Type start date: ")
         if check:
             date_in = input ("Type end date: ")
 
-        match date_in:
-            case r"^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$": #DAY
-            case r"^(19|20)\d\d[- /.](0[1-9]|1[012])$": #MONTH
-            case r"^(19|20)\d\d$": #YEAR
-            case -1: # NO LIMIT
-            case _: # INVALID
+        # Validate the input, if invalid loop back.
+        # see how to use re.compile to improve performance
+        if selection == 2 and re.match(r"^((19|20)\d\d|0000)[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$", date_in): # By Full Date
+            counter += 1
+            date_range.append(date_in)
+        elif selection == 1 and re.match(r"^((19|20)\d\d|0000)[- /.](0[1-9]|1[012])$", date_in): # By Month
+            counter += 1
+            date_range.append(date_in)
+        elif selection == 0 and re.match(r"^((19|20)\d\d|0000)$", date_in): # By Year
+            counter += 1
+            date_range.append(date_in)
+        else:
+            print("Invalid input, please make sure your input matches with formatting\n")
         
+        
+        # This is the idea to skip some unnecessary comparisons
+        if re.match(r"(^0000$|-00$|-00-)", date_range): # Can we just look at the tuple to match or do we have to go through each element
+            skip
+
+
         # THE LOGIC IS THIS, BUT MATE SUBTRACTING DATES ARE DUMB AS FUCK.
         # RATHER COMPARE YEARS THEN MONTHS THEN DAYS
         # BUT THAT IS A VERY UGLY CODE
         if check == 2:
-            if date_range[1] == -1:
-                OK
-            elif date_range[1] - date_range[0]:
+            if date_range[1] - date_range[0]:
                 OK
             else:
                 NOK
-
-
-    in_start = input("Type start date: ")
-    in_end = input("\nType end date: ")
-    date_range = (in_start,in_end)
 
     for i in data_list:
         i.iloc[:,9]
